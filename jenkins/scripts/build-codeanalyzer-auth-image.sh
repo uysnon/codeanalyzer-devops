2c0c3fd47f43c4208f3a56acf482216edcaa9d1a
uysnon <uysnon@gmail.com>
2022-09-09 09:18:07 +0300

    mvn clean package -> ./mvnw clean package

--- jenkins/scripts/build-codeanalyzer-auth-image.sh
+++ jenkins/scripts/build-codeanalyzer-auth-image.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-mvn -f $codeanalyzer_auth_path/pom.xml clean package
+mvn -f $codeanalyzer_auth_path/mvnw clean package
 docker build $codeanalyzer_auth_path -t uysnon/codeanalyzer-auth:latest
--- jenkins/scripts/build-codeanalyzer-metrics-image.sh
+++ jenkins/scripts/build-codeanalyzer-metrics-image.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-mvn -f $codeanalyzer_metrics_path/pom.xml clean package
+mvn -f $codeanalyzer_metrics_path/mvnw clean package
 docker build $codeanalyzer_metrics_path -t uysnon/codeanalyzer-metrics:latest
